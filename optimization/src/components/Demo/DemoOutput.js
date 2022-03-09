83988a3ec38f5b180f8ef4e082169a1f7f5201cf
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-03-09 14:35:15 +0530

    React.memo() function added

--- optimization/src/components/Demo/DemoOutput.js
+++ optimization/src/components/Demo/DemoOutput.js
@@ -1,10 +1,12 @@
+import React from "react";
 import MyParagraph from "./MyParagraph";
 
 const DemoOutput = (props) => {
   console.log("DemoOutput.js content here");
+
   return (
     <MyParagraph>{props.show ? "This is new Paragraph!" : ""}</MyParagraph>
   );
 };
 
-export default DemoOutput;
+export default React.memo(DemoOutput);
