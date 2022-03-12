19ae7f7bc6317cfa7178668d5cd14025744d7b24
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-03-12 17:22:18 +0530

    Error handling

--- http-request/src/App.js
+++ http-request/src/App.js
@@ -6,24 +6,48 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
     setLoading(true);
-    // const response = await fetch("https://swapi.py4e.com/api/films");
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
+    setError(null);
+    try {
+      // const response = await fetch("https://swapi.py4e.com/api/films");
+      const response = await fetch("https://swapi.dev/api/films");
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        releaseDate: movieData.release_date,
-        openingText: movieData.opening_crawl,
-      };
-    });
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
-    setMovies(transformedMovies);
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.release_date,
+          openingText: movieData.opening_crawl,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setLoading(false);
+  }
+
+  let content = <p>Found no movies</p>;
+
+  if (error) {
+    content = error;
+  }
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (loading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -31,11 +55,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!loading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!loading && movies.length === 0 && <p>Found no movies</p>}
-        {loading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </Fragment>
   );
 }
