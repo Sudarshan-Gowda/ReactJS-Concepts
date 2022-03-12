444a29ad7d5b847f868076365e8f1c2c36acf731
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-03-12 14:19:53 +0530

    loader adder

--- http-request/src/App.js
+++ http-request/src/App.js
@@ -5,23 +5,25 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            releaseDate: movieData.release_date,
-            openingText: movieData.opening_crawl,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  async function fetchMoviesHandler() {
+    setLoading(true);
+    // const response = await fetch("https://swapi.py4e.com/api/films");
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        releaseDate: movieData.release_date,
+        openingText: movieData.opening_crawl,
+      };
+    });
+
+    setMovies(transformedMovies);
+    setLoading(false);
   }
 
   return (
@@ -30,7 +32,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!loading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!loading && movies.length === 0 && <p>Found no movies</p>}
+        {loading && <p>Loading...</p>}
       </section>
     </Fragment>
   );
