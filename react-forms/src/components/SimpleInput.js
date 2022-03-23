e8876ba35c4008f01e2283c739a66c0a5dc799fd
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-03-23 10:58:20 +0530

    File changes: two ways to read the form input changes

--- react-forms/src/components/SimpleInput.js
+++ react-forms/src/components/SimpleInput.js
@@ -1,11 +1,31 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputRef = useRef();
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    console.log(nameInputRef.current.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={nameInputRef}
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+        />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
