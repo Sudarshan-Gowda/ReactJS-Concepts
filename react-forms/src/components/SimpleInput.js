a0ff5905c6e85cc7f5c24cf06aa8c396c8da258c
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-04-01 09:00:03 +0530

    File changes

--- react-forms/src/components/SimpleInput.js
+++ react-forms/src/components/SimpleInput.js
@@ -2,11 +2,16 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
-  //  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInValid = !enteredNameIsValid && enteredNameTouched;
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -35,7 +40,6 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input
-          // ref={nameInputRef}
           type='text'
           id='name'
           onChange={nameInputChangeHandler}
@@ -47,7 +51,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
