b0554b2480307da480a48b6cb943b38f40d031de
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-04-13 20:50:15 +0530

    File changes

--- food-order-app/src/components/UI/Modal.js
+++ food-order-app/src/components/UI/Modal.js
@@ -1,5 +1,35 @@
+import { Fragment } from "react";
+import ReactDom from "react-dom";
+
 import classes from "./Modal.module.css";
 
-const Modal = (props) => {};
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const protalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        protalElement
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        protalElement
+      )}
+    </Fragment>
+  );
+};
 
 export default Modal;
