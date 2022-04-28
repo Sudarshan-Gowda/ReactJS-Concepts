df9b9310626d7b27868ccc50705b92e15ffcc6ad
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-04-28 20:05:08 +0530

    File chages

--- react-router-practice/src/App.js
+++ react-router-practice/src/App.js
@@ -1,14 +1,18 @@
-import { NavLink, Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
     <Switch>
+      <Route path='/' exact>
+        <Redirect to='/quotes' />
+      </Route>
       <Route path='/quotes' exact>
         <AllQuotes />
       </Route>
-      <Route path='/qoutes/:quoteId'>
+      <Route path='/quotes/:quoteId'>
         <QuoteDetail />
       </Route>
       <Route path='/new-quote'>
--- react-router-practice/src/pages/QuoteDetail.js
+++ react-router-practice/src/pages/QuoteDetail.js
@@ -1,4 +1,21 @@
+import { Fragment } from "react";
+import { Route, useParams } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+
 const QuoteDetail = () => {
-  return <h1>Quote Detail Page</h1>;
+  const params = useParams();
+  const quoteId = params.quoteId;
+
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{quoteId}</p>
+      {/* path='/quotes/:quoteId/comments' */}
+      <Route path={`/quotes/${quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
 };
 export default QuoteDetail;
