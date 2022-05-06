933ec45cc5c5b12bf2cefb81f322fc80fae97524
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-05-06 21:50:51 +0530

    File changes

--- react-router-practice/src/pages/QuoteDetail.js
+++ react-router-practice/src/pages/QuoteDetail.js
@@ -5,8 +5,8 @@ import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 
 const DUMMY_QUOTES = [
-  { id: "q1", author: "Sudarshan", text: "Learning react is Fun!" },
-  { id: "q2", author: "Gowda", text: "Learning react is Great!" },
+  { id: "q1", author: "User1", text: "Learning react is Fun!" },
+  { id: "q2", author: "User2", text: "Learning react is Great!" },
 ];
 
 const QuoteDetail = () => {
