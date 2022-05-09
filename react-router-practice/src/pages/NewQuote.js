f9e4988fc563a0767f0a0f9f4dffbfe7e3a00c03
Sudarshan-Gowda <sudarshan.java.p@gmail.com>
2022-05-09 17:04:48 +0530

    File changes

--- react-router-practice/src/pages/NewQuote.js
+++ react-router-practice/src/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quote) => {
     console.log(quote);
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
